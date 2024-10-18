870111727716b54480b7afd6ed2d1fb79c913a6f
jaeseung choun <jaeseung_choun@tmax.co.kr>
2024-10-19 02:43:20 +0900

    refactor: 코드 리펙토링

--- src/main/java/com/humuson/ordermanager/entity/Order.java
+++ src/main/java/com/humuson/ordermanager/entity/Order.java
@@ -1,13 +1,18 @@
 package com.humuson.ordermanager.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDate;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Order {
   private String orderId;
   private String customerName;
   private LocalDate orderDate;
-  private OrderStatus orderStatus; // 처리 중, 배송 중, 완료
-
-  // 생성자, getter, setter 메서드 추가
+  private OrderStatus orderStatus;
 }
 

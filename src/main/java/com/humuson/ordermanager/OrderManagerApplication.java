7dcb43b5f8c5964917d8296ccba161981b68d05a
jaeseung choun <jaeseung_choun@tmax.co.kr>
2024-10-20 13:16:07 +0900

    feat: spring-retry 활성화

--- src/main/java/com/humuson/ordermanager/OrderManagerApplication.java
+++ src/main/java/com/humuson/ordermanager/OrderManagerApplication.java
@@ -2,8 +2,10 @@ package com.humuson.ordermanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
+@EnableRetry
 public class OrderManagerApplication {
 
   public static void main(String[] args) {
